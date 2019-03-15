ea49b46571000afa07f02f11297710330067abbd
ldgeao99 <ldgeao99@gmail.com>
2019-03-16 02:09:28 +0900

    원소의 개수를 세어 딕셔너리 형태로 반환하는 메소드 추가

---
+++ Test_Basic_Codes/2-1. make_element_count_dictionary.py
@@ -0,0 +1,27 @@
+"""<make_element_count_dictionary>
+- 리스트를 인자로 받아서 각각의 원소를 카운트 한 결과를 딕셔너리로 반환해준다.
+- count 메소드를 사용하면 똑같은 구간을 계속 탐색하는 비효율이 발생하여 효율이 떨어질 수 있다.
+"""
+
+
+def make_element_count_dictionary(lst):
+    dic = {}
+
+    for item in lst:
+        keys = dic.keys()
+        # 딕셔너리에 없다면 생성 후 1로 초기화
+        if item not in keys:
+            dic[item] = 1
+        # 딕셔너리에 있다면 카운트 1증가
+        else:
+            dic[item] += 1
+
+    return dic
+
+
+lst = ['a', 'b', 'c', 'c', 'd']
+print(make_element_count_dictionary(lst))  # {'a': 1, 'b': 1, 'c': 2, 'd': 1}
+
+lst = [1, 2, 2, 3, 4]
+print(make_element_count_dictionary(lst))  # {1: 1, 2: 2, 3: 1, 4: 1}
+
